1a21c53e67cf72cad3aacbc2afebd1ffb6bee7e0
Nich Bailey <nichbailey@gmail.com>
2017-10-14 15:31:00 +0000

    assignment-16: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,13 @@ posts = Post.all
         )
 end
 
+my_post = Post.find_or_create_by!(
+    title: "Nich Post",
+    body: "This is my unique post"
+    )
+    
+my_post.comments.find_or_create_by!(body: "This is my unique comment")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
